a61b915c5a2084809ed521bf1217ab42c06b3e21
H Michael Lord <Mike.Lord@WilyWare.com>
2021-07-07 17:32:19 -0500

    Code & Unit Test Added

--- src/ProviderQuality.Console/Program.cs
+++ src/ProviderQuality.Console/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace ProviderQuality.Console
 {
@@ -23,7 +24,8 @@ namespace ProviderQuality.Console
                     new Award {Name = "ACME Partner Facility", SellIn = 5, Quality = 7},
                     new Award {Name = "Blue Distinction Plus", SellIn = 0, Quality = 80},
                     new Award {Name = "Blue Compare", SellIn = 15, Quality = 20},
-                    new Award {Name = "Top Connected Providres", SellIn = 3, Quality = 6}
+                    new Award {Name = "Top Connected Providres", SellIn = 3, Quality = 6},
+                    new Award {Name = "Blue Star", SellIn = 10, Quality = 50 }
                 }
 
             };
@@ -33,18 +35,44 @@ namespace ProviderQuality.Console
             System.Console.ReadKey();
 
         }
-
+        /// <remarks>
+        /// Though it is not entirely obvious, SellIn must be the same as ExpiresIn.
+        /// The reason is SellIn decrements by one each time this method is invoked.
+        /// In other words, once SellIn reaches zero, that is the same as saying the
+        /// expiration date has passed. For example: if (Awards[i].SellIn < 0)
+        /// 
+        /// '# Your Assignment' says nothing about renaming this property or creating
+        /// a new ExpiresIn property, so I will leave it alone.  Although in the real-
+        /// world, I would at least question whether this should be renamed/refactored.
+        /// 
+        /// There is a requirement for a "Blue Star" Award 'grant date', but seeing awards
+        /// should lose quality value twice as fast as normal awards, it is not really
+        /// important what the actual date was. Thus there is no reason to persist it. If
+        /// on the other hand calculations where different before and after this date, then
+        /// we would have to persist it by adding it to the Award class.  For normal Awards
+        /// quality score degrades twice as fast after the expiration date passes, so "Blue
+        /// Star" should lose quality value twice as fast as normal awards, which means when
+        /// normal awards is doubled, "Blue Star" should be quadrupled.
+        /// </remarks>
         public void UpdateQuality()
         {
             for (var i = 0; i < Awards.Count; i++)
             {
                 if (Awards[i].Name != "Blue First" && Awards[i].Name != "Blue Compare")
                 {
+                    //The quality of an award is never negative
                     if (Awards[i].Quality > 0)
                     {
                         if (Awards[i].Name != "Blue Distinction Plus")
                         {
+                            //quality score degrades by one
                             Awards[i].Quality = Awards[i].Quality - 1;
+
+                            if (Awards[i].Quality > 0 && Awards[i].Name == "Blue Star")
+                            {
+                                //"Blue Star" awards should lose quality value twice as fast as normal awards
+                                Awards[i].Quality = Awards[i].Quality - 1;
+                            }
                         }
                     }
                 }
@@ -52,14 +80,17 @@ namespace ProviderQuality.Console
                 {
                     if (Awards[i].Quality < 50)
                     {
+                        //"Blue First" awards actually increase in quality the older they get
                         Awards[i].Quality = Awards[i].Quality + 1;
 
                         if (Awards[i].Name == "Blue Compare")
                         {
+                            //"Blue Compare", similar to "Blue First", increases in quality as the expiration date approaches;
                             if (Awards[i].SellIn < 11)
                             {
                                 if (Awards[i].Quality < 50)
                                 {
+                                    //Quality increases by 2 when there are 10 days or less left
                                     Awards[i].Quality = Awards[i].Quality + 1;
                                 }
                             }
@@ -68,6 +99,7 @@ namespace ProviderQuality.Console
                             {
                                 if (Awards[i].Quality < 50)
                                 {
+                                    //and by 3 where there are 5 days or less left
                                     Awards[i].Quality = Awards[i].Quality + 1;
                                 }
                             }
@@ -86,16 +118,25 @@ namespace ProviderQuality.Console
                     {
                         if (Awards[i].Name != "Blue Compare")
                         {
+                            //The quality of an award is never negative
                             if (Awards[i].Quality > 0)
                             {
                                 if (Awards[i].Name != "Blue Distinction Plus")
                                 {
+                                    //Once the expiration date has passed, quality score degrades twice as fast
                                     Awards[i].Quality = Awards[i].Quality - 1;
+
+                                    if (Awards[i].Quality > 0 && Awards[i].Name == "Blue Star")
+                                    {
+                                        //"Blue Star" awards should lose quality value twice as fast as normal awards
+                                        Awards[i].Quality = Awards[i].Quality - 1;
+                                    }
                                 }
                             }
                         }
                         else
                         {
+                            //quality value drops to 0 after the expiration date
                             Awards[i].Quality = Awards[i].Quality - Awards[i].Quality;
                         }
                     }
@@ -103,6 +144,7 @@ namespace ProviderQuality.Console
                     {
                         if (Awards[i].Quality < 50)
                         {
+                            //"Blue First" awards actually increase in quality the older they get
                             Awards[i].Quality = Awards[i].Quality + 1;
                         }
                     }
--- src/ProviderQuality.Tests/UpdateQualityAwardsTests.cs
+++ src/ProviderQuality.Tests/UpdateQualityAwardsTests.cs
@@ -8,6 +8,9 @@ namespace ProviderQuality.Tests
     [TestClass]
     public class UpdateQualityAwardsTests
     {
+        public const int GovQualityPlusQuality = 20;
+        public const int BlueStarQuality = 50;
+
         [TestMethod]
         public void TestImmutabilityOfBlueDistinctionPlus()
         {
@@ -26,6 +29,54 @@ namespace ProviderQuality.Tests
             app.UpdateQuality();
 
             Assert.IsTrue(app.Awards[0].Quality == 80);
+        }
+
+        [TestMethod]
+        public void TestImmutabilityOfBlueStar()
+        {
+            var app = new Program()
+            {
+                Awards = new List<Award>
+                {
+                    new Award {Name = "Gov Quality Plus", SellIn = 10, Quality = GovQualityPlusQuality},
+                    new Award {Name = "Blue Star", SellIn = 10, Quality = BlueStarQuality }
+                }
+            };
+
+            Assert.IsTrue(app.Awards.Count == 2);
+            Assert.IsTrue(app.Awards[0].Name == "Gov Quality Plus");
+            Assert.IsTrue(app.Awards[1].Name == "Blue Star");
+
+            for (int i = 1; i < BlueStarQuality; i++)
+            {
+                app.UpdateQuality();
+
+                Assert.IsTrue(app.Awards[0].Quality == (ComputeGovQualityPlusQuality(i, app.Awards[0]) < 0 ? 0 : ComputeGovQualityPlusQuality(i, app.Awards[0])));
+                Assert.IsTrue(app.Awards[1].Quality == (ComputeBlueStarQuantity(i, app.Awards[1]) < 0 ? 0 : ComputeBlueStarQuantity(i, app.Awards[1])));
+            }
+        }
+
+        protected int ComputeGovQualityPlusQuality(int idx, Award award)
+        {
+            return GovQualityPlusQuality - idx - (award.SellIn < 0 ? Math.Abs(award.SellIn) : 0);
+        }
+
+        protected int ComputeBlueStarQuantity(int idx, Award award)
+        {
+            int qty = BlueStarQuality - (idx * 2);
+            if (award.SellIn < 0)
+            {
+                int expireIn = (Math.Abs(award.SellIn) * 2);
+                if (qty > expireIn)
+                {
+                    qty -= expireIn;
+                }
+                else if ((qty - expireIn) < 0)
+                {
+                    qty = 0;
+                }
+            }
+            return qty;
         }
 
         // +++To Do - 1/10/2013: Discuss with team about adding more tests.  Seems like a lot of work for something
